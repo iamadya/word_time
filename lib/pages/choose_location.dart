028a9fd78fd00f6e700f91b2a67ad4cbb362eaa1
Adya Jha <callmeblackwidow02@gmail.com>
2023-10-10 23:07:20 +0530

    initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        foregroundColor: Colors.white,
+        title: Text(
+            'Choose a Location',
+        style: TextStyle(
+            color: Colors.white
+        ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:word_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading Screen'),
+    );
+  }
+}
